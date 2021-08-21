a8e4e1765900807fea632949fc4917a33a2f31e6
Golubova Viktoria <viki12007@inbox.ru>
2021-08-21 21:35:40 +0300

    filtering products by price

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -4,9 +4,10 @@
     <div class="v-catalog__link_to_cart">Cart: {{CART.length}}</div>
   </router-link>
    <h2>Catalog</h2>
+   <div class="filters">
     <v-select 
     :options="categories"
-    @select="selectCategory" 
+    @select="setFilter" 
     :selected="selected"
     />
     <!-- <v-select 
@@ -17,6 +18,27 @@
     Изменение состояния приложения в зависимости от 
     изменения ширины экрана
     /> -->
+    <div class="range-slider">
+      <input 
+      type="range" 
+      min="0" 
+      max="1000" 
+      step="10"
+      v-model.number="minPrice"
+      @change="setRangeSlider">
+      <input 
+      type="range" 
+      min="0" 
+      max="1000" 
+      step="10"
+      v-model.number="maxPrice"
+      @change="setRangeSlider">
+    </div>
+    <div class="range-values">
+      <p>Min: {{minPrice}}</p>
+      <p>Max: {{maxPrice}}</p>
+    </div>
+    </div>
    <div class="v-catalog__list">
    <v-catalog-item 
    v-for="product in filteredProducts"
@@ -47,7 +69,9 @@ export default {
             {name: 'Женские', value: 'woman'}
           ],
           selected: 'Все',
-          sortedProducts: []
+          sortedProducts: [],
+          minPrice: 0,
+          maxPrice: 1000
     }
    },
    computed: {
@@ -62,7 +86,7 @@ export default {
          return this.sortedProducts
        }
        else {
-         return this.PRODUCTS
+         return []
        }
      }
    },
@@ -74,19 +98,36 @@ export default {
      addToCart(data) {
        this.ADD_TO_CART(data);
      },
-    selectCategory(option) { 
-      this.sortedProducts = this.PRODUCTS.filter((product) => {
-        return product.category === option.name
+    setFilter(option) {
+      this.sortedProducts = [...this.PRODUCTS];
+      if(option) {
+        this.selected = option.name;
+      }
+      this.sortedProducts = this.sortedProducts.filter(item => {
+        return item.price >= this.minPrice && item.price <= this.maxPrice;
       })
-      this.selected = option.name;
-    }
-   },
+      if (this.selected !== 'Все') {
+        this.sortedProducts = this.sortedProducts.filter(product => {
+        return product.category === this.selected;
+        })
+      }
+    },
+    setRangeSlider() {
+      if (this.minPrice > this.maxPrice) {
+        let tmp = this.maxPrice;
+        this.maxPrice = this.minPrice;
+        this.minPrice = tmp;
+      }
+      this.setFilter();
+     }
+    },
    watch: {},
    mounted() {
        this.GET_PRODUCTS_FROM_API()
        .then((response) => {
           if(response.data) {
-            console.log('Data arrived!')
+            console.log('Data arrived!');
+            this.setFilter();
           }
        })
    }
@@ -108,5 +149,27 @@ export default {
       padding: 16px;
       border: 1px solid lightgray;
     }
-} 
+}
+.filters {
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+ }
+ .range-slider {
+   width: 200px;
+   margin: auto 16px;
+   text-align: center;
+   position: relative;
+ }
+ .range-slider svg, .range-slider input[type=range] {
+   position: absolute;
+   left: 0;
+   bottom: 0;
+ }
+ input[type=range]::-webkit-slider-thumb {
+   z-index: 2;
+   position: relative;
+   top: 2px;
+   margin-top: -7px;
+ }
 </style>
